18a4e3dc73653a4a5bd5619c9a35f5db0e19c756
Jonas Horstmann <jonas.horstmann2804@gmail.com>
2016-04-16 16:12:09 +0200

    für timo

---
+++ app/src/main/java/hfu/tango/example/camera/MainActivity.java
@@ -0,0 +1,111 @@
+package hfu.tango.example.camera;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoCameraPreview;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+import com.projecttango.tangosupport.TangoSupport;
+
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    /*private final ArrayList<TangoCoordinateFramePair> framePairs =
+            new ArrayList<>();*/
+
+    private final UpdatableBlockingBuffer<FloatBuffer> pointBuffer =
+            new UpdatableBlockingBuffer<>();
+
+    private Tango tango;
+    private TangoCameraPreview cameraPreview;
+    private TangoCameraIntrinsics cameraIntrinsics;
+    private OverlayView overlayView;
+    private TangoPoseData poseData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cameraPreview = (TangoCameraPreview) findViewById(R.id.cameraPreview);
+        tango = new Tango(this);
+        overlayView = (OverlayView) findViewById(R.id.overlayView);
+        cameraIntrinsics = tango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        cameraPreview.connectToTangoCamera(tango, TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+        try {
+            connectTango();
+        } catch (TangoOutOfDateException e) {
+            e.printStackTrace();
+        }
+        Log.d("debug",String.valueOf(cameraIntrinsics.calibrationType));
+
+        //pointParser = new PointCloudParser(pointBuffer, out, this);
+        //pointParser.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        try {
+            tango.disconnect();
+        } catch (TangoErrorException e) {
+            e.printStackTrace();
+        }
+        cameraPreview.disconnectFromTangoCamera();
+        //pointParser.interrupt();
+    }
+
+    private void connectTango() {
+        TangoConfig tangoConfig = tango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        tangoConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+        tangoConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+        tango.connect(tangoConfig);
+
+        final ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<>();
+
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+
+        tango.connectListener(framePairs, new OnTangoUpdateListener() {
+
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
+                overlayView.update(xyzIj, cameraIntrinsics, tango.getPoseAtTime(xyzIj.timestamp, framePairs.get(0)));
+                overlayView.postInvalidate();
+            }
+
+            @Override
+            public void onFrameAvailable(int cameraId) {
+                if(cameraId == TangoCameraIntrinsics.TANGO_CAMERA_COLOR)
+                    cameraPreview.onFrameAvailable();
+
+            }
+
+            @Override
+            public void onPoseAvailable(final TangoPoseData pose) {}
+
+            @Override
+            public void onTangoEvent(final TangoEvent event) {}
+        });
+    }
+}
---
+++ app/src/main/java/hfu/tango/example/camera/OverlayView.java
@@ -0,0 +1,132 @@
+package hfu.tango.example.camera;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+import com.projecttango.tangosupport.TangoSupport;
+
+import java.nio.FloatBuffer;
+
+public class OverlayView extends View {
+    private Paint paint;
+    private TangoSupport.DepthBuffer buffer;
+    private TangoXyzIjData data;
+    private TangoPoseData poseData;
+    private TangoCameraIntrinsics cameraIntrinsics;
+    private boolean isSet = false;
+
+    public OverlayView(Context context) {
+        super(context);
+
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(5);
+    }
+
+    public OverlayView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStrokeWidth(5);
+    }
+
+    /*
+
+    public void update(FloatBuffer buffer, TangoCameraIntrinsics intrinsics) {
+        this.buffer = buffer;
+        this.intrinsics = intrinsics;
+    }
+
+    private float[] translateBuffer(FloatBuffer pBuffer) {
+        Log.d("debug", "calibration type: " + String.valueOf(intrinsics.calibrationType));
+        float[] drawBuffer = new float[pBuffer.capacity() * 2 / 3];
+
+        float fx = (float) intrinsics.fx;
+        float fy = (float) intrinsics.fy;
+        float cx = (float) intrinsics.cx;
+        float cy = (float) intrinsics.cy;
+
+        float k1 = (float) intrinsics.distortion[0];
+        float k2 = (float) intrinsics.distortion[1];
+        float k3 = (float) intrinsics.distortion[2];
+
+        for(int i = 0, j = 0; i < pBuffer.capacity(); i += 3, j += 2) {
+
+            float x = pBuffer.get(i);
+            float y = pBuffer.get(i + 1);
+            float z = pBuffer.get(i + 2);
+            float ru = (float) ((Math.sqrt(Math.pow(x,2) + Math.pow(y,2)) / Math.pow(z,2)));
+            float rd = (float) (ru + k1 * Math.pow(ru,3) + k2 * Math.pow(ru,5) + k3 * Math.pow(ru,7));
+
+            float rd2 = (float) (1 / k1 * Math.atan(2 * ru * Math.tan(k1)/2));
+
+            drawBuffer[j] = (x / z * fx * rd / ru + cx);
+            drawBuffer[j + 1] = (x / z * fy * rd / ru + cy);
+
+            Log.d("debug", "ru: " + String.valueOf(ru) + " rd: " + String.valueOf(rd) + " fx: "
+                    + String.valueOf(fx) + " cx: " + String.valueOf(cx));
+
+            Log.d("debug", "[METER] x: " + String.valueOf(pBuffer.get(i)) + " y: " +
+                    String.valueOf(pBuffer.get(i + 1)) + " z: " + String.valueOf(pBuffer.get(i + 2)) +
+            " ratio: " + String.valueOf(pBuffer.get(i) / pBuffer.get(i + 1)));
+
+            Log.d("debug", "[KOORDINATEN] x: " + String.valueOf(drawBuffer[j]) +  " y: " + String.valueOf(drawBuffer[j + 1]) +
+            " ratio: " + String.valueOf(drawBuffer[j] / drawBuffer[j + 1]));
+        }
+        return drawBuffer;
+    }
+    */
+
+    public void update(TangoXyzIjData data, TangoCameraIntrinsics cameraIntrinsics, TangoPoseData poseData) {
+        if (!isSet) isSet = true;
+        this.data = data;
+        this.cameraIntrinsics = cameraIntrinsics;
+        this.poseData = poseData;
+    }
+
+    private int zToColor(float z) {
+        if (z * 50 >= 255)
+            return Color.rgb(1, 0, 0);
+        else
+            return Color.rgb((int) (255 - z * 50), 0, 0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+
+        //paint.setColor(paint.getColor() == Color.WHITE ? Color.BLACK : Color.WHITE);
+
+        if (isSet) {
+            for (float u = 0; u <= 1f; u += 0.01) {
+                for (float v = 0; v <= 1f; v += 0.01) {
+                    float[] point = TangoSupport.getDepthAtPointNearestNeighbor(data, cameraIntrinsics, poseData, u, v);
+                    /*if(point != null) {
+                        Log.d("debug",
+                                "x: " + String.valueOf(point[0]) +
+                                        " y: " + String.valueOf(point[1]) +
+                                        " z: " + String.valueOf(point[2])); */
+                    if (point != null) {
+                        paint.setColor(zToColor(point[2]));
+                    } else {
+                        paint.setColor(Color.WHITE);
+                    }
+                    canvas.drawPoint(u * 1920, v * 1200, paint);
+                }
+            }
+            //canvas.drawPoints(translateBuffer(buffer), paint);
+            // canvas.drawPoints(translateBuffer(buffer), paint);
+
+        }
+    }
+}
