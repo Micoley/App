50cd830bbcbca9b68eb02ffda0e6c9eb391b4153
VincentKa <vincent.kattinger@hs-furtwangen.de>
2016-05-01 21:18:24 +0200

    Vervollständigung
    Beide Algorithmen vervollständigt, kalibriert und in Funktionen mit
    sinnvollem Input/Output gepackt.

--- Tangotest/src/Main.java
+++ Tangotest/src/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 import java.awt.image.DataBufferByte;
 
 import javax.imageio.ImageIO;
@@ -36,81 +35,100 @@ class Main {
 		    }
 		 return bufImage;
 	}
-	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
+	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); 
+	}
+	private Mat getMat(BufferedImage img){
+		Mat m = new Mat(img.getHeight(), img.getWidth(), CvType.CV_8UC3);
+		byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+		m.put(0, 0, pixels);
+		return m;
+	}
+	
+	public List<MatOfPoint> contours(BufferedImage img){
+		Mat mask = new Mat();
+		Scalar minValues;
+		Scalar maxValues;
+		List<MatOfPoint> contours;
+		List<MatOfPoint> output = new ArrayList<MatOfPoint>();
+		Mat hierarchy = new Mat();
+		Mat m = getMat(img);
+		
+		Imgproc.cvtColor(m, m, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.blur(m, m, new Size(3, 3));
+		 
+		for(int i = 0; i <= 190; i = i + 64){
+			contours = new ArrayList<MatOfPoint>();
+			minValues = new Scalar(i, i, i);
+			maxValues = new Scalar(i+64, i+64, i+64);
+			Core.inRange(m, minValues, maxValues, mask);
+			  
+			Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_NONE);
+			  
+			if (hierarchy.size().height > 0 && hierarchy.size().width > 0){
+				for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0]){
+					if(contours.get(idx).total() > 30){
+						output.add(contours.get(idx));
+						//Ausgabe
+						/*for (int y = 0; y < contours.get(idx).total(); y++) { 
+							for (int z = 0; z < contours.get(idx).cols(); z++) {
+								double[] vec = contours.get(idx).get(y, z);
+						        for(double a : vec){
+						        	System.out.print(a + " ");
+						        }
+						        System.out.println(" ");
+							}
+						}
+						Imgproc.drawContours(m, contours, idx, new Scalar(0, 255, 0), 1);*/
+					}
+				}
+			}
+		}
+		return output;
+	}
+	public Mat houghLinesP(BufferedImage img){
+		Mat lines = new Mat();
+		Mat m = getMat(img);
+		
+		Imgproc.cvtColor(m, m, Imgproc.COLOR_BGR2GRAY);
+		Imgproc.Canny(m, m, 30, 600);
+		Imgproc.GaussianBlur(m, m, new Size(3,3), 0);
+		Imgproc.HoughLinesP(m, lines, 1, Math.PI/180, 100, 100, 20);
+		//Ausgabe
+		/*for (int y = 0; y < lines.total(); y++) {
+			for (int x = 0; x < lines.cols(); x++) {
+				double[] vec = lines.get(y, x);
+					Imgproc.line(frame, new Point(vec[0],vec[1]), new Point(vec[2],vec[3]), new Scalar(0,255,0), 2);
+			}
+		}
+		Imgproc.cvtColor(m, m, Imgproc.COLOR_GRAY2BGR);*/
+		return lines;
+	}
 	
 	public static void main(String[] args) {
-		  int x1 = 1;
-		  Mat mask = new Mat();
-		  
-		  BufferedImage img = null;
-		  try {
-			    img = ImageIO.read(new File("test3.jpg"));
-			} catch (IOException e) {
-				System.out.print("fail");
-			}
-		  Mat frame = new Mat(img.getHeight(), img.getWidth(), CvType.CV_8UC3);
-		  byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
-		  frame.put(0, 0, pixels);
-		  Imgproc.cvtColor(frame,frame,Imgproc.COLOR_BGR2GRAY);
-		  //Imgproc.cvtColor(frame, frame, Imgproc.COLOR_BGR2HSV);
-		  
-		  switch (x1){
-		  case 0:
-			  //FindContours
-			  Imgproc.blur(frame, frame, new Size(3, 3));
-			  Scalar minValues = new Scalar(0, 100, 100);
-			  Scalar maxValues = new Scalar(100, 255, 255);
-			 
-			  Core.inRange(frame, minValues, maxValues, mask);
-			  
-			  List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-			  Mat hierarchy = new Mat();
-			  
-			  Imgproc.findContours(mask, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-			  
-			  Imgproc.cvtColor(frame,frame,Imgproc.COLOR_GRAY2BGR);
-			  
-			  if (hierarchy.size().height > 0 && hierarchy.size().width > 0)
-			  {
-				  for (int idx = 0; idx >= 0; idx = (int) hierarchy.get(0, idx)[0])
-			      {
-					  System.out.println("check");
-			          Imgproc.drawContours(frame, contours, idx, new Scalar(0, 255, 0), 2);
-			      }
-			  }
-		  break;
-		  //HoughLinesP
-		  
-		  //Imgproc.GaussianBlur(frame, frame, new Size(5,5), 0);
-		  //Imgproc.adaptiveThreshold(frame, frame, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY, 5, 0);
-		  //Core.bitwise_not(frame, frame);
-		  case 1:
-			  Imgproc.Canny(frame,frame,150,600);
-	
-			  Mat lines = new Mat();
-			  Mat copy = frame.clone();
-			  Imgproc.HoughLinesP(copy, lines, 1, Math.PI/180, 200, 100, 20);
-			  Imgproc.cvtColor(frame,frame,Imgproc.COLOR_GRAY2BGR);
-			  System.out.println(lines.total());
-			  for (int x = 0; x < lines.cols(); x++) {
-			        double[] vec = lines.get(0, x);
-			        for(double i : vec){
-			        	System.out.println(i);
-			        }
-			        Imgproc.line(frame, new Point(vec[0],vec[1]), new Point(vec[2],vec[3]), new Scalar(0,255,0), 2);
-			        System.out.println("check");
-			  }
-			  break;
-			  default: break;
-		  }
-		  
-		  
-		  img = getImage(frame, ".jpg");
-		  try {
-			    ImageIO.write(img, "jpg", new File("saved.jpg"));
-			} catch (IOException e) {
-			}
-
-	  }
-
+		Main main = new Main();
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("test3.jpg"));
+		} catch (IOException e) {
+			System.out.print("Datei konnte nicht geladen werden.");
+		};
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		Mat houghLinesP = new Mat();
+		
+		contours = main.contours(img);
+		houghLinesP = main.houghLinesP(img);
+		
+		for(Mat x : contours){
+			System.out.println(x.toString());
+		}
+		System.out.println("_________________________");
+		System.out.println(houghLinesP.toString());
+		
+		//Ausgabe als Datei
+		/*img = getImage(m, ".jpg");
+		try {
+			ImageIO.write(img, "jpg", new File("saved.jpg"));
+		} catch (IOException e) {
+		}*/
+	}
 }
