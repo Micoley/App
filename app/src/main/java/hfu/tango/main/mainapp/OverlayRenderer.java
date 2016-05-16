ec4f26a07ac3bb22d93c9a34c36547a5c4a3e8fd
Jonas Horstmann <jonas.horstmann2804@gmail.com>
2016-05-17 01:25:49 +0200

    Tango-Funktionalität + OverlayView eingefügt

--- app/src/main/java/hfu/tango/main/mainapp/OverlayRenderer.java
+++ app/src/main/java/hfu/tango/main/mainapp/OverlayRenderer.java
@@ -1,17 +1,89 @@
 package hfu.tango.main.mainapp;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
-public class OverlayRenderer extends View implements Runnable {
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
 
+import java.nio.FloatBuffer;
+
+public class OverlayRenderer extends View implements Runnable {
+    private Paint paint;
+    private FloatBuffer buffer;
+    private TangoCameraIntrinsics intrinsics;
+
+    {
+        paint = new Paint();
+        paint.setStrokeWidth(5);
+    }
 
     public OverlayRenderer(Context context) {
         super(context);
     }
 
-    @Override
-    public void run() {
-
+    public OverlayRenderer(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
+
+    /**
+     * Die Overlayview wird mit den neuen Informationen des Tiefensensors geupdatet
+     */
+    public void update(FloatBuffer buffer, TangoCameraIntrinsics intrinsics) {
+        Log.d("HFU_DEBUG", "update() aufgerufen");
+        this.buffer = buffer;
+        this.intrinsics = intrinsics;
+    }
+
+    /**
+     * Berechnet aus den Tiefeninformationen die betroffenen Pixel auf dem Kamerabild
+     * @param canvas Die Canvas auf der gezeichnet wird
+     */
+    private void drawPointBuffer(Canvas canvas) {
+        float fx = (float) intrinsics.fx;
+        float fy = (float) intrinsics.fy;
+        float cx = (float) intrinsics.cx;
+        float cy = (float) intrinsics.cy;
+        float w = (float) intrinsics.width;
+        float h = (float) intrinsics.height;
+
+        for (int i = 0, j = 0; i < buffer.capacity(); i += 3, j += 2) {
+            float x = buffer.get(i);
+            float y = buffer.get(i + 1);
+            float z = buffer.get(i + 2);
+
+            paint.setColor(zToColor(z));
+            canvas.drawPoint((x * fx + z * cx) / z * (this.getWidth() / w), (y * fy + z * cy) / z * (this.getHeight() / h), paint);
+        }
+    }
+
+    /**
+     * Bildet aus einem reellen Wert die Farbe ab
+     * @param z Ein z-Wert des Tiefenbildes (0-5.0)
+     * @return Eine Farbe von rgb(0,0,0) bis rgb(255,255,0)
+     */
+
+    private int zToColor(float z){
+        int R = (int)(255 * z) / 5;
+        int G = (int)(255 * (5 - z)) / 5;
+        return  Color.rgb(R,G,0);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Log.d("HFU_DEBUG", "onDraw() aufgerufen");
+
+        if (buffer != null) {
+            drawPointBuffer(canvas);
+        }
+    }
+
+    @Override
+    public void run() {}
 }
--- app/src/main/java/hfu/tango/main/mainapp/TangoActivity.java
+++ app/src/main/java/hfu/tango/main/mainapp/TangoActivity.java
@@ -3,7 +3,19 @@ package hfu.tango.main.mainapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.WindowManager;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.Tango.OnTangoUpdateListener;
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -11,7 +23,25 @@ import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
 
+import java.util.ArrayList;
+
 public class TangoActivity extends Activity {
+
+    /**
+     * Wird benötigt um auf die Project Tango spezifischen Sensoren zuzugreifen
+     */
+    private Tango mTango;
+
+    /**
+     * Einstellungen und Informationen zu den einzelnen Kameras
+     */
+    private TangoCameraIntrinsics mCameraIntrinsics;
+
+    /**
+     * Anzeigen der Punktewolke
+     */
+    private OverlayRenderer mOverlayRenderer;
+
     /**
      * Die View auf der das Kamerabild angezeigt wird
      */
@@ -24,7 +54,7 @@ public class TangoActivity extends Activity {
 
         @Override
         public void onManagerConnected(int status) {
-            switch(status) {
+            switch (status) {
                 case BaseLoaderCallback.SUCCESS:
                     mOpenCvCameraView.enableView();
                     break;
@@ -43,11 +73,21 @@ public class TangoActivity extends Activity {
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.openCvCameraView);
         mOpenCvCameraView.setCvCameraViewListener(new OpenCvCameraListener());
+        mOverlayRenderer = (OverlayRenderer) findViewById(R.id.overlayRenderer);
+        mTango = new Tango(this);
+        mCameraIntrinsics = mTango.getCameraIntrinsics(TangoCameraIntrinsics.TANGO_CAMERA_DEPTH);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        try {
+            connectTango();
+        } catch (TangoOutOfDateException e) {
+            e.printStackTrace();
+        }
+
         /* sorgt dafür, dass die Verbindung zum OpenCV-Manager sowie der Aufruf des Callbacks
            asynchron stattfinden
          */
@@ -58,17 +98,80 @@ public class TangoActivity extends Activity {
     protected void onPause() {
         super.onPause();
 
-        if(mOpenCvCameraView != null)
+        try {
+            mTango.disconnect();
+        } catch (TangoErrorException e) {
+            e.printStackTrace();
+        }
+
+        if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        if(mOpenCvCameraView != null)
+        try {
+            mTango.disconnect();
+        } catch (TangoErrorException e) {
+            e.printStackTrace();
+        }
+
+        if (mOpenCvCameraView != null) {
             mOpenCvCameraView.disableView();
+        }
     }
+
+    /**
+     * Einstellen der Tango-Sensorik und Erzeugen des Listeners,
+     * der auf Tango-Events reagiert
+     */
+    private void connectTango() {
+        TangoConfig tangoConfig = mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT);
+        tangoConfig.putBoolean(TangoConfig.KEY_BOOLEAN_DEPTH, true);
+        tangoConfig.putBoolean(TangoConfig.KEY_BOOLEAN_MOTIONTRACKING, true);
+        mTango.connect(tangoConfig);
+
+        final ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<>();
+
+        framePairs.add(new TangoCoordinateFramePair(
+                TangoPoseData.COORDINATE_FRAME_START_OF_SERVICE,
+                TangoPoseData.COORDINATE_FRAME_DEVICE));
+
+        Log.d("HFU_DEBUG", "Listener connected");
+        mTango.connectListener(framePairs, new OnTangoUpdateListener() {
+
+            /**
+             * Wird aufgerufen wenn neue Daten des Tiefensensors verfügbar sind
+             * @param xyzIj Enthält die Entfernungsangaben in Meter in der Form: x1,y1,z1,x2,y2,z2...
+             */
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
+                Log.d("HFU_DEBUG", "onXyz..  ausgerufen");
+                mOverlayRenderer.update(xyzIj.xyz, mCameraIntrinsics);
+                mOverlayRenderer.postInvalidate();
+            }
+
+            /**
+             * Wird aufgerufen wenn ein neues Kamerabild verfügbar ist
+             * @param cameraId Die ID der jeweiligen Kamera
+             */
+            @Override
+            public void onFrameAvailable(int cameraId) {
+            }
+
+            @Override
+            public void onPoseAvailable(final TangoPoseData pose) {
+            }
+
+            @Override
+            public void onTangoEvent(final TangoEvent event) {
+            }
+        });
+    }
+
 
     /**
      * Klasse die auf Events des OpenCvCameraViews reagiert
@@ -76,13 +179,16 @@ public class TangoActivity extends Activity {
     private class OpenCvCameraListener implements CvCameraViewListener2 {
 
         @Override
-        public void onCameraViewStarted(int width, int height) {}
+        public void onCameraViewStarted(int width, int height) {
+        }
 
         @Override
-        public void onCameraViewStopped() {}
+        public void onCameraViewStopped() {
+        }
 
         /**
          * wird aufgerufen wenn ein neues Bild der Kamera verfügbar ist
+         *
          * @param inputFrame das neue Frame
          * @return die Matrix-Darstellung des Kamerabildes in RGBA bzw. Graustufen
          */
