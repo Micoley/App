24bcd5862a74e4d39862d7028c3a27ef20e64811
jh2804 <jonas.horstmann@hs-furtwangen.de>
2016-06-04 12:34:57 +0200

    automatischer ndk-build geht jetzt sowohl auf Windows als auch auf UNIX

--- app/src/main/obj/local/armeabi-v7a/objs/FrameBuffer/FrameBuffer.o.d
+++
@@ -1,5 +0,0 @@
-D:/Workspace/Android/Projects/App/app/src/main/obj/local/armeabi-v7a/objs/framebuffer/FrameBuffer.o: \
- D:/Workspace/Android/Projects/App/app/src/main/jni/FrameBuffer.cpp \
- D:/Workspace/Android/Projects/App/app/src/main/jni/tango_client_api.h
-
-D:/Workspace/Android/Projects/App/app/src/main/jni/tango_client_api.h:
